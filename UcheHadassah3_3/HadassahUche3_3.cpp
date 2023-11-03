d3d157fe82faf090955eefd8a1757593a1662b16
Hadassah Uche <uhadassah1@gmail.com>
2023-11-03 12:34:27 +0200

    Added the loops to read in from the user

---
+++ UcheHadassah3_3/HadassahUche3_3.cpp
@@ -0,0 +1,34 @@
+/*Name:Uche Hadassah
+This program reads in two lists of positive integers and checks if they're identical or not*/
+#include<iostream>
+using namespace std;
+int main()
+{
+	const int MAX = 100;
+	int List1[MAX];
+	int List2[MAX];
+	int i = 0;
+	int j = 0;
+	cout << "Enter List 1(-1 to finish):";
+	do
+	{
+		cin >> List1[i];
+		if (List1[i] == -1)
+		{
+			break;
+		}
+		i++;
+	} while (List1[i] != -1);
+	cout << "Enter List 2(-1 to finish):";
+	do
+	{
+		cin >> List2[j];
+		if (List1[j] == -1)
+		{
+			break;
+		}
+		j++;
+	} while (List2[j] != -1);
+	
+	return 0;
+}
